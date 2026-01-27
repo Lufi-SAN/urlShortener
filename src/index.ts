efa6e9722fd69c461dfaca48686acb3961201401
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2026-01-27 23:14:33 +0100

    feat: enhance index.ts with route imports and type annotations

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
-import express, { type NextFunction } from 'express';
+import express, { type NextFunction, type Request, type Response, type ErrorRequestHandler } from 'express';
 import dotenv from 'dotenv';
-
+import getHelpRoute from './routes/get-help.route.js';
+import createURIRoute from './routes/create-uri.route.js';
+import callURIRoute from './routes/call-uri.route.js';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get('/', getHelpRoute);
+app.post('/create', createURIRoute);
+app.get('/:shortUri', callURIRoute);
 
-app.use((req, res, next) => {//Not Found Handler
+app.use((req : Request, res : Response, next : NextFunction) => {//Not Found Handler
 
 })
 
-app.use((err, req, res, next) => {//Error Handler
+const errorHandler : ErrorRequestHandler = (err : Error, req : Request, res : Response, next : NextFunction) => {}
 
-})
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
