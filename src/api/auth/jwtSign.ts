320a39d079642278f7d5624e89846f01b12f7108
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2026-02-09 12:56:00 +0100

    feat: add JWT signing functionality and extend Request type for user data

---
+++ src/api/auth/jwtSign.ts
@@ -0,0 +1,34 @@
+import { SignJWT } from "jose";
+import dotenv from "dotenv";
+dotenv.config();
+
+export async function jwtSign(id: number, tokenVersion: number) {
+    const accessSecretKey = new TextEncoder().encode(process.env.ACCESS_JWT_SECRET_KEY);
+    const refreshSecretKey = new TextEncoder().encode(process.env.REFRESH_JWT_SECRET_KEY);
+    const alg = "HS256";
+    const jti = crypto.randomUUID();
+
+    const issuer = "url-shortener-api";
+    const audience = "url-shortener-users";
+
+    const accessJWT = await new SignJWT({})
+    .setProtectedHeader({ alg })
+    .setExpirationTime("15m")
+    .setIssuedAt()
+    .setSubject(id.toString())
+    .setIssuer(issuer)
+    .setAudience(audience)
+    .sign(accessSecretKey);
+
+    const refreshJWT = await new SignJWT({tokenVersion : tokenVersion.toString()})
+    .setProtectedHeader({ alg })
+    .setExpirationTime("7d")
+    .setIssuedAt()
+    .setSubject(id.toString())
+    .setIssuer(issuer)
+    .setAudience(audience)
+    .setJti(jti)
+    .sign(refreshSecretKey);
+
+    return { accessJWT, refreshJWT, jti };
+}
--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -2,6 +2,10 @@ import "express";
 
 declare module "express-serve-static-core" {
   interface Request {
+    userData?: {
+      id: number;
+      token_version: number;
+    },
     rateLimit?: {
       limit: number;
       used: number;
