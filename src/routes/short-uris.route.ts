cf1bd803def296f200d55b80309b62ef984dbd79
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2026-02-10 11:13:58 +0100

    feat: add short URIs route and remove create URI route

---
+++ src/routes/short-uris.route.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const shortURISRoute = Router();
+
+export default shortURISRoute;
+
+// POST   /v1/short-urls
+// GET    /v1/short-urls/:id
+// PATCH  /v1/short-urls/:id
+// DELETE /v1/short-urls/:id
+// GET    /v1/short-urls   (list)
