e2771fd786762533bfe5875b7755658ebe11a397
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2026-02-16 12:22:00 +0100

    refactor: simplify checkUserDetailsStore function by removing password validation

--- src/api/store/check-user-details-service/check-user-details.store.ts
+++ src/api/store/check-user-details-service/check-user-details.store.ts
@@ -1,18 +1,9 @@
 import { pg } from "../../connections/postgres.connection.js";
-import bcrypt from 'bcrypt';
 
 export async function checkUserDetailsStore(username: string, password: string) {
     const query = 'SELECT id, password_hash, token_version FROM users WHERE username = $1';
     const values = [username];
     
     const res = await pg.query(query, values);
-    if (res.rows.length === 0) {
-        return null; // User not found
-    }
-    const {id, password_hash, token_version} = res.rows[0];
-    const isPasswordValid = await bcrypt.compare(password, password_hash);
-    if (!isPasswordValid) {
-        return null; // Invalid password
-    }
-    return { id, token_version };
+    return res.rows[0]
 }
