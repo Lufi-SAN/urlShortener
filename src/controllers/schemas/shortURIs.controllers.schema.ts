b488a842ad8cda74c0fec52b29181c7884932d30
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2026-03-23 15:42:46 +0100

    feat: create query schema for zod validation on query input for GET /short-uris route

--- src/controllers/schemas/shortURIs.controllers.schema.ts
+++ src/controllers/schemas/shortURIs.controllers.schema.ts
@@ -1,32 +1,38 @@
 import { z } from 'zod'
 
+export const cursorQuerySchema = z.object({
+  cursor: z.coerce.number().int().positive(),
+  firstPage: z.enum(['true', 'false']).optional(),
+  direction: z.optional(z.string().toLowerCase().pipe(z.literal('prev')))
+})
+
 export const userShortURIDataSchema = z.object({
     original_uri: z.string().trim().transform(val => { 
-  if (!val) return null
-  const lower = val.toLowerCase()
+      if (!val) return null
+      const lower = val.toLowerCase()
 
-  if (lower.startsWith("/")) {
-    if (lower.startsWith("//") && !lower.startsWith("///")) {
-        return new URL("https:" + val).href
-    } else return null
-  } else { 
-    if (lower.startsWith("http://") || lower.startsWith("https://")) {
-      return new URL(val).href
-    } else return new URL("https://" + val).href
-  }
-}).pipe(z.url({
-  protocol: /^https?$/,
-  hostname: z.regexes.domain
-})),
+      if (lower.startsWith("/")) {
+        if (lower.startsWith("//") && !lower.startsWith("///")) {
+          return new URL("https:" + val).href
+        } else return null
+      } else { 
+        if (lower.startsWith("http://") || lower.startsWith("https://")) {
+        return new URL(val).href
+        } else return new URL("https://" + val).href
+      }
+    }).pipe(z.url({
+        protocol: /^https?$/,
+        hostname: z.regexes.domain
+    })),
     is_private: z.boolean(),
     expires_at: z.string().trim().transform(val => {
-  if (!val) return null
-  let userDate = new Date(val) 
-  const minDate = new Date(Date.now() + (60 * 60 * 1000))
+      if (!val) return null
+      let userDate = new Date(val) 
+      const minDate = new Date(Date.now() + (60 * 60 * 1000))
 
-  if (userDate < minDate) return null
-  return userDate.toISOString()
-}).pipe(z.iso.datetime())
+      if (userDate < minDate) return null
+      return userDate.toISOString()
+    }).pipe(z.iso.datetime())
 })
 
 export type userShortURIData = z.infer<typeof userShortURIDataSchema>
